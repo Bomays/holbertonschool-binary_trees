00d5e3b7ee257369031980730ec5757b96ee3599
Bomays <9314@holbertonstudents.com>
2024-08-05 17:39:18 +0200

    fixing branch conflict added tasks 9, 10, 11 compiled 'binary tree Sizeheight/depth/height', updated header library in tasks 0,1,2 files & put all prototypes in header file

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -13,7 +13,7 @@
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	int depth = 0;
-	
+
 	if (tree == NULL)
 	{
 		return (0);
--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,15 +1,34 @@
 #include <stdlib.h>
 #include <stdio.h>
+<<<<<<< HEAD
 #include "binary_trees.h"                                                                                                                                                                                           /**                                                                                                        * binary_tree_size - function that measures the size of a binary tree
  *                                                                                                         * @tree: pointer to the root node of the tree to measur the size
  *                                                                                                         * Return: return 0 if tree is NULL or return the size of the tree if succeeded
+=======
+#include "binary_trees.h"
+/**
+ * binary_tree_size - function that measures the size of a binary tree
+ *
+ * @tree: pointer to the root node of the tree to measur the size
+ *
+ * Return: return 0 if tree is NULL or return the size of the tree if succeeded
+>>>>>>> a8b1f96 (added tasks 9, 10, 11 compiled 'binary tree Sizeheight/depth/height', updated header library in tasks 0,1,2 files & put all prototypes in header file)
  *
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
+<<<<<<< HEAD
         if (tree == NULL)
         {
                 return (0);
         }
 
 	return (binary_tree_size(tree->right) + binary_tree_size(tree->left)+1);
+=======
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	return (binary_tree_size(tree->right) + binary_tree_size(tree->left) + 1);
+}
+>>>>>>> a8b1f96 (added tasks 9, 10, 11 compiled 'binary tree Sizeheight/depth/height', updated header library in tasks 0,1,2 files & put all prototypes in header file)
