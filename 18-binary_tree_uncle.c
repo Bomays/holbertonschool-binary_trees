e6674266b0f08f057594e7d7f939ae8ac4cf8e04
Bomays <9314@holbertonstudents.com>
2024-08-07 21:49:22 +0200

    modified binary_tree_t *parent declaration in binary_tree_uncle function so node and parent's and grandparent's nodes are checked if NULL first - iso c90 std

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -44,12 +44,14 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	binary_tree_t *parent = node->parent;
+	binary_tree_t *parent;
 
 	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 	{
 		return (NULL);
 	}
+
+	parent = node->parent;
 
 	return (binary_tree_sibling(parent));
 }
